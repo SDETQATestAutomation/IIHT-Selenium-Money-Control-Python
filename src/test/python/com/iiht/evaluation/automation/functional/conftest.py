d31d07f899a90d5e8d8d90f755cfa414a5152e54
pranjan <Prashant.Ranjan@gartner.com>
2023-11-20 22:36:52 +0530

    added fixture

---
+++ src/test/python/com/iiht/evaluation/automation/functional/conftest.py
@@ -0,0 +1,29 @@
+import pytest
+
+
+@pytest.fixture(scope="class")
+def class_fixture():
+    print("\nBefore class setup")
+    yield
+    print("\nAfter class setup")
+
+
+@pytest.fixture()
+def method_fixture():
+    print("\nBefore method setup")
+    yield
+    print("\nAfter method setup")
+
+
+@pytest.fixture()
+def data():
+    return ['data1', 'data2', 'data3']
+
+
+@pytest.fixture(params=[
+    {'user': 'user1', 'pass': 'pass1'},
+    {'user': 'user2', 'pass': 'pass2'},
+    {'user': 'user3', 'pass': 'pass3'}
+])
+def data_provider(request):
+    return request.param
---
+++ src/test/python/com/iiht/evaluation/automation/functional/temp/testpytest.py
@@ -0,0 +1,25 @@
+import pytest
+
+
+@pytest.mark.usefixtures('class_fixture')
+@pytest.mark.usefixtures('conf_setup')
+@pytest.mark.usefixtures('data')
+class TestDemo:
+
+    def test_class_method1(self):
+        print('class test method1 execution')
+
+    def test_class_method2(self):
+        print('class test method2 execution')
+
+    def test_class_method3(self, data):
+        print('class test method3 execution')
+        print(data[0])
+        print(data[1])
+        print(data[2])
+
+    @pytest.mark.usefixtures('data_provider')
+    def test_class_method4(self, data_provider):
+        print('class test method3 execution')
+        print(data_provider['user'])
+        print(data_provider['pass'])
