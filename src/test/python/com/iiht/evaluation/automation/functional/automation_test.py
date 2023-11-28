a2f810897524ddf0c1572fefe759b20276fb8f94
pranjan <Prashant.Ranjan@gartner.com>
2023-11-28 18:34:58 +0530

    Added use case for fixed deposit calculator

--- src/test/python/com/iiht/evaluation/automation/functional/automation_test.py
+++ src/test/python/com/iiht/evaluation/automation/functional/automation_test.py
@@ -132,7 +132,7 @@ def wait_for_element_present(driver, xpath):
     try:
         for i in range(count_needed):
             all_elements = driver.find_elements(By.XPATH, xpath)
-            all_elements_length = all_elements.size()
+            all_elements_length = len(all_elements)
             if all_elements_length > 0:
                 element_present = True
                 break
@@ -155,7 +155,7 @@ def wait_for_element_visible(driver, xpath):
     try:
         for i in range(count_needed):
             all_elements = driver.find_elements(By.XPATH, xpath)
-            all_elements_length = all_elements.size()
+            all_elements_length = len(all_elements)
             if all_elements_length > 0:
                 required_element = all_elements[0]
                 required_element_display = required_element.is_displayed()
@@ -182,7 +182,7 @@ def wait_for_element_not_visible(driver, xpath):
     try:
         for i in range(count_needed):
             all_elements = driver.find_elements(By.XPATH, xpath)
-            all_elements_length = all_elements.size()
+            all_elements_length = len(all_elements)
             if all_elements_length > 0:
                 required_element = all_elements[0]
                 required_element_display = required_element.is_displayed()
@@ -198,6 +198,13 @@ def wait_for_element_not_visible(driver, xpath):
     finally:
         driver.implicitly_wait(15)
         return element_not_visible
+
+
+def do_javascript_click(driver, element):
+    try:
+        driver.execute_script("arguments[0].click();", element)
+    except Exception as ex:
+        print(f"ex {ex}")
 
 
 def open_login_panel(driver):
@@ -340,6 +347,255 @@ def check_logged_in_user(driver, email):
     return is_email_checked
 
 
+def select_submenu_from_menu(driver, main_menu, sub_menu):
+    main_menu = money_control_element["main_menu"].replace("$(main_menu)", main_menu)
+    sub_menu = money_control_element["sub_menu"].replace("$(sub_menu)", sub_menu)
+
+    main_menu_element = find_element_use_xpath(driver, main_menu)
+    if main_menu_element is None:
+        return False
+    main_menu_element_visible = wait_for_element_visible(driver, main_menu)
+    print(f"main_menu_element_visible {main_menu_element_visible}")
+    if main_menu_element_visible is False:
+        return False
+    actions = ActionChains(driver)
+    actions.move_to_element(main_menu_element).perform()
+    sub_menu_element = find_element_use_xpath(driver, sub_menu)
+    if sub_menu_element is None:
+        return False
+    sub_menu_element_visible = wait_for_element_visible(driver, sub_menu)
+    print(f"sub_menu_element_visible {sub_menu_element_visible}")
+    if sub_menu_element_visible is False:
+        return False
+    sub_menu_element.click()
+    check_page_load_complete(driver)
+    return True
+
+
+def fixed_deposit_calculator_enter_investment_amount(driver, investment_amount):
+    investment_amount_input = money_control_element["investment_amount_input"]
+    investment_amount_input_element = find_element_use_xpath(driver, investment_amount_input)
+    if investment_amount_input_element is None:
+        return False
+    investment_amount_input_element_visible = wait_for_element_visible(driver, investment_amount_input)
+    print(f"investment_amount_input_element_visible {investment_amount_input_element_visible}")
+    if investment_amount_input_element_visible is False:
+        return False
+    investment_amount_input_element.clear()
+    investment_amount_input_element.send_keys(investment_amount)
+    return True
+
+
+def fixed_deposit_calculator_check_investment_amount(driver, investment_amount):
+    investment_amount_input = money_control_element["investment_amount_input"]
+    investment_amount_input_element = find_element_use_xpath(driver, investment_amount_input)
+    if investment_amount_input_element is None:
+        return False
+    investment_amount_input_element_visible = wait_for_element_visible(driver, investment_amount_input)
+    print(f"investment_amount_input_element_visible {investment_amount_input_element_visible}")
+    if investment_amount_input_element_visible is False:
+        return False
+    investment_amount_input_element_value_attribute = investment_amount_input_element.get_attribute("value")
+    print(f"investment_amount_input_element_value_attribute {investment_amount_input_element_value_attribute}")
+    if investment_amount_input_element_value_attribute != investment_amount:
+        return False
+    return True
+
+
+def fixed_deposit_calculator_enter_investment_period(driver, investment_period):
+    investment_period_input = money_control_element["investment_period_input"]
+    investment_period_input_element = find_element_use_xpath(driver, investment_period_input)
+    if investment_period_input_element is None:
+        return False
+    investment_period_input_element_visible = wait_for_element_visible(driver, investment_period_input)
+    print(f"investment_period_input_element_visible {investment_period_input_element_visible}")
+    if investment_period_input_element_visible is False:
+        return False
+    investment_period_input_element.clear()
+    investment_period_input_element.send_keys(investment_period)
+    return True
+
+
+def fixed_deposit_calculator_check_investment_period(driver, investment_period):
+    investment_period_input = money_control_element["investment_period_input"]
+    investment_period_input_element = find_element_use_xpath(driver, investment_period_input)
+    if investment_period_input_element is None:
+        return False
+    investment_period_input_element_visible = wait_for_element_visible(driver, investment_period_input)
+    print(f"investment_period_input_element_visible {investment_period_input_element_visible}")
+    if investment_period_input_element_visible is False:
+        return False
+    investment_period_input_element_value_attribute = investment_period_input_element.get_attribute("value")
+    print(f"investment_period_input_element_value_attribute {investment_period_input_element_value_attribute}")
+    if investment_period_input_element_value_attribute != investment_period:
+        return False
+    return True
+
+
+def fixed_deposit_calculator_enter_rate_of_return(driver, rate_of_return):
+    rate_of_return_input = money_control_element["rate_of_return_input"]
+    rate_of_return_input_element = find_element_use_xpath(driver, rate_of_return_input)
+    if rate_of_return_input_element is None:
+        return False
+    rate_of_return_input_element_visible = wait_for_element_visible(driver, rate_of_return_input)
+    print(f"rate_of_return_input_element_visible {rate_of_return_input_element_visible}")
+    if rate_of_return_input_element_visible is False:
+        return False
+    rate_of_return_input_element.clear()
+    rate_of_return_input_element.send_keys(rate_of_return)
+    return True
+
+
+def fixed_deposit_calculator_check_rate_of_return(driver, rate_of_return):
+    rate_of_return_input = money_control_element["rate_of_return_input"]
+    rate_of_return_input_element = find_element_use_xpath(driver, rate_of_return_input)
+    if rate_of_return_input_element is None:
+        return False
+    rate_of_return_input_element_visible = wait_for_element_visible(driver, rate_of_return_input)
+    print(f"rate_of_return_input_element_visible {rate_of_return_input_element_visible}")
+    if rate_of_return_input_element_visible is False:
+        return False
+    rate_of_return_input_element_value_attribute = rate_of_return_input_element.get_attribute("value")
+    print(f"rate_of_return_input_element_value_attribute {rate_of_return_input_element_value_attribute}")
+    if rate_of_return_input_element_value_attribute != rate_of_return:
+        return False
+    return True
+
+
+def fixed_deposit_calculator_select_interest_frequency(driver, interest_frequency):
+    interest_frequency_radio = money_control_element["interest_frequency_radio"].replace("$(interest_frequency)",
+                                                                                         interest_frequency)
+    interest_frequency_radio_element = find_element_use_xpath(driver, interest_frequency_radio)
+    if interest_frequency_radio_element is None:
+        return False
+    do_javascript_click(driver, interest_frequency_radio_element)
+    interest_frequency_radio_checked_attribute = interest_frequency_radio_element.get_attribute("checked")
+    print(f"interest_frequency_radio_checked_attribute {interest_frequency_radio_checked_attribute}")
+    if interest_frequency_radio_checked_attribute != "checked":
+        return False
+    return True
+
+
+def fixed_deposit_calculator_enter_tax_rate(driver, tax_rate):
+    tax_rate_input = money_control_element["tax_rate_input"]
+    tax_rate_input_element = find_element_use_xpath(driver, tax_rate_input)
+    if tax_rate_input_element is None:
+        return False
+    tax_rate_input_element_visible = wait_for_element_visible(driver, tax_rate_input)
+    print(f"tax_rate_input_element_visible {tax_rate_input_element_visible}")
+    if tax_rate_input_element_visible is False:
+        return False
+    tax_rate_input_element.clear()
+    tax_rate_input_element.send_keys(tax_rate)
+    return True
+
+
+def fixed_deposit_calculator_check_tax_rate(driver, tax_rate):
+    tax_rate_input = money_control_element["tax_rate_input"]
+    tax_rate_input_element = find_element_use_xpath(driver, tax_rate_input)
+    if tax_rate_input_element is None:
+        return False
+    tax_rate_input_element_visible = wait_for_element_visible(driver, tax_rate_input)
+    print(f"tax_rate_input_element_visible {tax_rate_input_element_visible}")
+    if tax_rate_input_element_visible is False:
+        return False
+    tax_rate_input_element_value_attribute=tax_rate_input_element.get_attribute("value")
+    print(f"tax_rate_input_element_value_attribute {tax_rate_input_element_value_attribute}")
+    if tax_rate_input_element_value_attribute != tax_rate:
+        return False
+    return True
+
+
+def fixed_deposit_calculator_click_submit_button(driver):
+    submit_button = money_control_element["submit_button"]
+    submit_button_element = find_element_use_xpath(driver, submit_button)
+    if submit_button_element is None:
+        return False
+    submit_button_element_visible = wait_for_element_visible(driver, submit_button)
+    print(f"submit_button_element_visible {submit_button_element_visible}")
+    if submit_button_element_visible is False:
+        return False
+    submit_button_element.click()
+    return True
+
+
+def fixed_deposit_calculator_click_reset_button(driver):
+    reset_button = money_control_element["reset_button"]
+    reset_button_element = find_element_use_xpath(driver, reset_button)
+    if reset_button_element is None:
+        return False
+    reset_button_element_visible = wait_for_element_visible(driver, reset_button)
+    print(f"reset_button_element_visible {reset_button_element_visible}")
+    if reset_button_element_visible is False:
+        return False
+    reset_button_element.click()
+    return True
+
+
+def fixed_deposit_calculator_check_total_payment(driver, total_payment):
+    total_payment_span = money_control_element["total_payment_span"]
+    total_payment_span_element = find_element_use_xpath(driver, total_payment_span)
+    if total_payment_span_element is None:
+        return False
+    total_payment_span_element_visible = wait_for_element_visible(driver, total_payment_span)
+    print(f"total_payment_span_element_visible {total_payment_span_element_visible}")
+    if total_payment_span_element_visible is False:
+        return False
+    total_payment_span_element_text = total_payment_span_element.text
+    print(f"total_payment_span_element_text {total_payment_span_element_text}")
+    if total_payment_span_element_text != total_payment:
+        return False
+    return True
+
+
+def fixed_deposit_calculator_check_total_interest(driver, total_interest):
+    total_interest_span = money_control_element["total_interest_span"]
+    total_interest_span_element = find_element_use_xpath(driver, total_interest_span)
+    if total_interest_span_element is None:
+        return False
+    total_interest_span_element_visible = wait_for_element_visible(driver, total_interest_span)
+    print(f"total_interest_span_element_visible {total_interest_span_element_visible}")
+    if total_interest_span_element_visible is False:
+        return False
+    total_interest_span_element_text = total_interest_span_element.text
+    print(f"total_interest_span_element_text {total_interest_span_element_text}")
+    if total_interest_span_element_text != total_interest:
+        return False
+    return True
+
+
+def fixed_deposit_calculator_check_total_corpus(driver, total_corpus):
+    total_corpus_span = money_control_element["total_corpus_span"]
+    total_corpus_span_element = find_element_use_xpath(driver, total_corpus_span)
+    if total_corpus_span_element is None:
+        return False
+    total_corpus_span_element_visible = wait_for_element_visible(driver, total_corpus_span)
+    print(f"total_corpus_span_element_visible {total_corpus_span_element_visible}")
+    if total_corpus_span_element_visible is False:
+        return False
+    total_corpus_span_element_text = total_corpus_span_element.text
+    print(f"total_corpus_span_element_text {total_corpus_span_element_text}")
+    if total_corpus_span_element_text != total_corpus:
+        return False
+    return True
+
+
+def fixed_deposit_calculator_check_post_tax_amount(driver, post_tax_amount):
+    post_tax_amount_span = money_control_element["post_tax_amount_span"]
+    post_tax_amount_span_element = find_element_use_xpath(driver, post_tax_amount_span)
+    if post_tax_amount_span_element is None:
+        return False
+    post_tax_amount_span_element_visible = wait_for_element_visible(driver, post_tax_amount_span)
+    print(f"post_tax_amount_span_element_visible {post_tax_amount_span_element_visible}")
+    if post_tax_amount_span_element_visible is False:
+        return False
+    post_tax_amount_span_element_text = post_tax_amount_span_element.text
+    print(f"post_tax_amount_span_element_text {post_tax_amount_span_element_text}")
+    if post_tax_amount_span_element_text != post_tax_amount:
+        return False
+    return True
+
+
 def test_login(driver):
     # open_login_panel(driver)
     # open_signin_box(driver)
@@ -347,4 +603,20 @@ def test_login(driver):
     # signin_box_enter_password(driver, "igetup@7AM")
     # signin_box_click_login_button(driver)
     # signin_box_check_error(driver, "Invalid User Id/EmailID or Password. Please try again.")
-    check_logged_in_user(driver,"Prashant.ranjan.qa@gmail.com")
+    # check_logged_in_user(driver, "Prashant.ranjan.qa@gmail.com")
+    # select_submenu_from_menu(driver, "Personal Finance", "Fixed Deposit Interest Calculator")
+    # fixed_deposit_calculator_enter_investment_amount(driver, 300000)
+    # fixed_deposit_calculator_enter_investment_period(driver, 5)
+    # fixed_deposit_calculator_enter_rate_of_return(driver, 12)
+    # fixed_deposit_calculator_select_interest_frequency(driver, "Monthly")
+    # fixed_deposit_calculator_enter_tax_rate(driver, 15)
+    # fixed_deposit_calculator_click_submit_button(driver)
+    # fixed_deposit_calculator_check_total_payment(driver, "300,000.00")
+    # fixed_deposit_calculator_check_total_interest(driver, "245,009.01")
+    # fixed_deposit_calculator_check_total_corpus(driver, "545,009.01")
+    # fixed_deposit_calculator_check_post_tax_amount(driver, "506,787.60")
+    # fixed_deposit_calculator_check_investment_amount(driver,"10000000")
+    # fixed_deposit_calculator_check_investment_period(driver,"10")
+    # fixed_deposit_calculator_check_rate_of_return(driver,"10")
+    # fixed_deposit_calculator_check_tax_rate(driver,"15")
+
--- src/test/python/com/iiht/evaluation/automation/locators/object_repository.py
+++ src/test/python/com/iiht/evaluation/automation/locators/object_repository.py
@@ -9,4 +9,20 @@ money_control_element = {
     "signin_box_login_button": "//div[@id='mc_login']/descendant::button[@type='submit']",
     "logged_in_user_link": "//div[contains(@class,'user_after_login')]/descendant::a[@class='userlink']",
     "signin_box_error_div": "//div[@id='mc_login']/descendant::div[contains(@class,'mclog_error')]",
+    "main_menu": "//div[contains(@class,'bottom_nav')]/descendant::li[contains(@class,'menu_l')]/a[@title='$(main_menu)']",
+    "sub_menu": "//ul[contains(@class,'market_listmenu')]/descendant::a[@title='$(sub_menu)']",
+    "investment_amount_input": "//input[@id='edulonvalue_1']",
+    "investment_period_input": "//input[@id='edulonvalue_2']",
+    "rate_of_return_input": "//input[@id='edulonvalue_3']",
+    "interest_frequency_radio": "//span[text()='$(interest_frequency)']/preceding-sibling::input[@type='radio']",
+    "tax_rate_input": "//input[@id='edulonvalue_4']",
+    "submit_button": "//a[text()='Submit']",
+    "reset_button": "//a[text()='Reset']",
+    "total_payment_span": "//div[@class='result_block']/following-sibling::div/descendant::div[@class='subContent' and text()='Total Payment']/preceding-sibling::span[1]",
+    "total_interest_span": "//div[@class='result_block']/following-sibling::div/descendant::div[@class='subContent' and text()='Total Interest']/preceding-sibling::span[1]",
+    "total_corpus_span": "//div[@class='result_block']/following-sibling::div/descendant::div[@class='subContent' and text()='Total Corpus']/preceding-sibling::span[1]",
+    "post_tax_amount_span": "//div[@class='result_block']/following-sibling::div/descendant::div[@class='subContent' and text()='Post Tax Amount']/preceding-sibling::span[1]",
+
+
+
 }
